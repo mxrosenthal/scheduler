0b3db9d29939c4be7789c2b53b26b8abe643c51e
Max Rosenthal <mxrosenthal@yahoo.com>
2020-03-09 10:49:44 -0700

    added verification to make sure an interviewer is selected before saving an interview. throws an error if not. now the whole app won't crash.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,61 +3,73 @@ import Button from '../Button';
 import InterviewerList from '../InterviewerList';
 
 export default function Form(props) {
-
   const { name, interviewers, interviewer, onSave, onCancel, create } = props;
 
-  const [currentName, setName] = useState(name || '')
-  const [currentInterviewer, setInterviewer] = useState(interviewer || null)
-  const [error, setError] = useState("");
+  const [currentName, setName] = useState(name || '');
+  const [currentInterviewer, setInterviewer] = useState(interviewer || null);
+  const [error, setError] = useState('');
 
-  const reset = function () {
+  const reset = function() {
     setError('');
     setName('');
     setInterviewer(null);
-  }
+  };
 
-  const cancel = function () {
+  const cancel = function() {
     setError('');
     reset();
     onCancel();
-  }
+  };
 
-  const save = function () {
+  const save = function() {
     reset();
     onSave(currentName, currentInterviewer, create);
-  }
+  };
 
   function validate() {
     if (currentName === '') {
       setError('Student name cannot be blank');
       return;
     }
+    //added this validation step to make sure an interviewer gets selected.
+    if (!currentInterviewer) {
+      setError('Must select an interviewer.');
+      return;
+    }
     setError('');
-    save(currentName, currentInterviewer, create)
+    save(currentName, currentInterviewer, create);
   }
 
   return (
-    <main className="appointment__card appointment__card--create">
-      <section className="appointment__card-left">
-        <form autoComplete="off">
+    <main className='appointment__card appointment__card--create'>
+      <section className='appointment__card-left'>
+        <form autoComplete='off'>
           <input
-            className="appointment__create-input text--semi-bold"
-            name="name"
-            type="text"
+            className='appointment__create-input text--semi-bold'
+            name='name'
+            type='text'
             value={currentName}
-            placeholder="Enter Student Name"
-            onChange={(event) => setName(event.target.value)}
+            placeholder='Enter Student Name'
+            onChange={event => setName(event.target.value)}
             onSubmit={event => event.preventDefault()}
-            data-testid="student-name-input"
+            data-testid='student-name-input'
           />
         </form>
-        <section className="appointment__validation">{error}</section>
-        <InterviewerList interviewers={interviewers} value={currentInterviewer} onChange={setInterviewer} />
+        <section className='appointment__validation'>{error}</section>
+        <InterviewerList
+          interviewers={interviewers}
+          value={currentInterviewer}
+          onChange={setInterviewer}
+        />
       </section>
-      <section className="appointment__card-right">
-        <section className="appointment__actions">
-          <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={validate}>Save</Button>
+      <section className='appointment__card-right'>
+        <section className='appointment__actions'>
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={validate}>
+            Save
+          </Button>
         </section>
       </section>
     </main>
